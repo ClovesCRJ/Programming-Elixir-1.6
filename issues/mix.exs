e46cf7e4f75fc68a5c65cd5efd49c93a998075d7
Cloves Rodrigues <cloves.crj@gmail.com>
2021-08-08 18:32:44 -0300

    Chapter 14

--- issues/mix.exs
+++ issues/mix.exs
@@ -10,6 +10,13 @@ defmodule Issues.MixProject do
       source_url:      "https://github.com/ClovesCRJ/Programming-Elixir-1.6",
       elixir:          "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls:           :test,
+        "coveralls.details": :test,
+        "coveralls.post":    :test,
+        "coveralls.html":    :test,
+      ],
       deps:            deps()
     ]
   end
@@ -24,8 +31,9 @@ defmodule Issues.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      { :httpoison, "~> 1.0.0"  },
-      { :poison,    "~> 3.1"    },
+      { :httpoison,   "~> 1.0.0"              },
+      { :poison,      "~> 3.1"                },
+      { :excoveralls, "~> 0.5.5", only: :test },
     ]
   end
 
